cb8f270697e97b48116972e41d59ac65f5f801bb
leo <jaykang1992@naver.com>
2024-09-25 01:49:07 +0900

    적립 명령 금액에 대한 검증 로직을 작성한다

--- src/test/kotlin/io/hhplus/tdd/point/ChargePointCommandValidatorTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/ChargePointCommandValidatorTest.kt
@@ -45,4 +45,27 @@ class ChargePointCommandValidatorTest {
         // when, then
         assertDoesNotThrow { sut.validate(command) }
     }
+
+    @Test
+    @DisplayName("포인트 충전 명령의 금액이 1 보다 작으면, 에러를 반환한다")
+    fun `when amount in charge point command is less than 1, then throw error`() {
+        // given
+        val id = 1L
+        val amount = -Random.nextLong(1, Long.MAX_VALUE)
+        val command = ChargePointCommand(id, amount)
+
+        // when, then
+        assertThrows<RuntimeException> { sut.validate(command) }
+    }
+
+    @Test
+    @DisplayName("포인트 충전 명령의 금액이 1 이상이면, 성공한다")
+    fun `when amount in charge point command is greater or equal to 1, then succeed`() {
+        val id = 1L
+        val amount = Random.nextLong(1, Long.MAX_VALUE)
+        val command = ChargePointCommand(id, amount)
+
+        // when, then
+        assertDoesNotThrow { sut.validate(command) }
+    }
 }
