a3e78f3757692123af2e4f8c54c1dedacdf2907a
leo <jaykang1992@naver.com>
2024-09-26 02:54:02 +0900

    사용 명령을 내릴때 잔고에 대한 검증 로직을 작성한다

--- src/test/kotlin/io/hhplus/tdd/dummy/DummyUsePointCommandValidator.kt
+++ src/test/kotlin/io/hhplus/tdd/dummy/DummyUsePointCommandValidator.kt
@@ -2,4 +2,4 @@ package io.hhplus.tdd.dummy
 
 import io.hhplus.tdd.point.validator.UsePointCommandValidator
 
-class DummyUsePointCommandValidator : UsePointCommandValidator()
+class DummyUsePointCommandValidator : UsePointCommandValidator(DummyUserPointRepository())
--- src/test/kotlin/io/hhplus/tdd/point/UsePointCommandValidatorTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/UsePointCommandValidatorTest.kt
@@ -1,16 +1,21 @@
 package io.hhplus.tdd.point
 
 import io.hhplus.tdd.point.command.UsePointCommand
+import io.hhplus.tdd.point.model.UserPoint
+import io.hhplus.tdd.point.ports.UserPointRepository
 import io.hhplus.tdd.point.validator.UsePointCommandValidator
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import kotlin.random.Random
 
 class UsePointCommandValidatorTest {
 
-    private val sut = UsePointCommandValidator()
+    private val mockUserPointRepository = mock(UserPointRepository::class.java)
+    private val sut = UsePointCommandValidator(mockUserPointRepository)
 
     @Test
     @DisplayName("사용 명령의 유저 아이디가 0이면, 에러를 반환한다")
@@ -43,6 +48,34 @@ class UsePointCommandValidatorTest {
         val id = Random.nextLong(1, Long.MAX_VALUE)
         val amount = 100L
         val command = UsePointCommand(id, amount)
+        val stubUserPoint = UserPoint(id, 1000L, System.currentTimeMillis())
+        Mockito.`when`(mockUserPointRepository.getById(id)).thenReturn(stubUserPoint)
+
+        // when, then
+        assertDoesNotThrow { sut.validate(command) }
+    }
+
+    @Test
+    @DisplayName("사용 명령을 내릴떄 잔고가 부족하면, 에러를 반환한다")
+    fun `when use amount but point is not enough, then throw error`() {
+        // given
+        val id = 1L
+        val amount = 100L
+        val command = UsePointCommand(id, amount)
+
+        // when, then
+        assertThrows<RuntimeException> { sut.validate(command) }
+    }
+
+    @Test
+    @DisplayName("사용 명령을 내릴때 잔고가 충분하면, 성공한다")
+    fun `when`() {
+        // given
+        val id = Random.nextLong(1, Long.MAX_VALUE)
+        val amount = 100L
+        val command = UsePointCommand(id, amount)
+        val stubUserPoint = UserPoint(id, 1000L, System.currentTimeMillis())
+        Mockito.`when`(mockUserPointRepository.getById(id)).thenReturn(stubUserPoint)
 
         // when, then
         assertDoesNotThrow { sut.validate(command) }
